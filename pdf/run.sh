70246269f7602bdfc2f5d8461764f4c08435da9f
Qazi Hamid <qazirashid@gmail.com>
2020-02-16 11:33:37 +0000

    minor changes

--- pdf/run.sh
+++ pdf/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-pdf2txt.py -t xml -A ./sample/hello.pdf | grep text\ |\
+pdf2txt.py -t xml -A $1 | grep text\ |\
 tr -d '\"/' | tr '=<>' ' '|\
-awk 'BEGIN{FS=" ";OFS="|" ;print "font,size, x1,x2,y1,y2,char"}
+awk 'BEGIN{FS=" ";OFS="," ;print "font,size,y1,y2,x1,x2,char"}
      	{ split($5,position,",");
-	 print $3,$7,position[2],position[4],position[1], position[3],$8 }'
+	 print $3,$7,position[2],position[4],position[1], position[3],$8 }'|\
+awk -F, 'NF==7{print $0}' > $2
