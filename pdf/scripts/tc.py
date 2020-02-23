9c823d41310ec68be65051d241a49abb2b5be271
Qazi Hamid <qazirashid@gmail.com>
2020-02-23 01:05:17 +0000

    more work on table candidates

---
+++ pdf/scripts/tc.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# This script will mark words that show possiblity of being in a table.
+# input is a csv file with words extracted using 'pdftocsv.sh'.
+# The output is another csv with more columns that are useful for detecting tables.
+
+import pandas as pd
+import numpy as np
+import sys
+
+df = pd.read_csv(sys.argv[1])
+# Mark line breaks
+linebreak = (df['yspace'] < 0).replace({True:1, False:0});
+df['linebreak'] = linebreak;
+#udw will inicate unusual distance between two words
+#ud_th is the threshold for flagging the distance as unusual
+ud_th = 5;
+udw = (df['xspace']> ud_th).replace({True:1, False:0});
+df['udw'] = udw;
+
+# next we check whether a word is the first word on a line
+
+fwol = np.roll(linebreak,1);
+fwol[0] = 1;
+
+# We check the distance of first word from the right margin.
+# We limit it to only thos words that are first on their line.
+drm = df['wx1']*fwol;
+#next define a threshold that check whether drm is normal. 
+# To be calculated later. 
+drm_th = 150; 
+udrm = (drm > drm_th).replace({True:1, False:0})
+df['udrm'] = udrm;
+
+tc = udw+udrm;
+df['tc'] = tc;
+g = (df.groupby('y2').sum())['tc']
+
+df.to_csv(sys.argv[2])
+g.to_csv(sys.argv[2] + ".tablecandidates.csv")
+
+
+
+
+ 
+
+
+
+
+
--- pdf/scripts/words.py
+++ pdf/scripts/words.py
@@ -5,8 +5,19 @@ import numpy as np
 import sys
 
 space_th = 1 ; # space threshold
+# dfr is the data frame of chars as created by pdfminer
 dfr = pd.read_csv(sys.argv[1])
+#df is the sorted version of dfr. The sorting brings the words in the correct order. 
 df = dfr.sort_values(by=['y2','x1'], ascending=[False, True])
+# The index of df is still inherited from dfr which as become unusable for later algorithms because of sorting. So we rest index to ensure that frames in df are ordered by a monotonically increasing index.
+
+df.reset_index(drop=True, inplace=True);
+#Debug step. remove if no issues are found.
+sname = sys.argv[2] + ".s.csv";
+df.to_csv(sname);
+#debug end
+
+# calculate inter-character space
 xspace = np.roll(df['x1'],-1) - df['x2'];
 yspace = np.roll(df['y1'],-1) - df['y1'];
 # calculate word boundaries
@@ -20,6 +31,7 @@ num_records = df.shape[0];
 words = df['char'].copy();
 wx1 = df['x1'].copy();
 
+#If a group of characters has distance between characters less than a threshold, then assemble those characters in words.
 
 while(i<num_records):
   charsum=df.iloc[i].char
@@ -38,7 +50,11 @@ df['words'] = words;
 df['wx1']  = wx1;
 df['wb'] = wb
 
+df.to_csv(sys.argv[2] + ".w.csv")
+
+# drop partial words and keep complete words only
 dw = df[df['wb']]
+#drop unwanted columns to declutter the csv.
 dw= dw.drop(columns=['x1', 'char','concat', 'wb'])
 dw.to_csv(sys.argv[2])
 
